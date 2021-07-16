05bfb3ddb7dd035973a4f9568385c3be847803e1
Danny Nguyen <dnguyen@genetec.com>
2021-07-16 12:00:03 -0400

    Added unit test for listing charts images with values

--- helm-utils/list-images/suite_test.go
+++ helm-utils/list-images/suite_test.go
@@ -1,6 +1,7 @@
 package list_images
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -19,4 +20,34 @@ func TestListImages(t *testing.T) {
 	junitReporter := reporters.NewJUnitReporter(toFile)
 
 	RunSpecsWithDefaultAndCustomReporters(t, "List Images Suite", []Reporter{junitReporter})
+}
+
+func TestListImagesWithValues(t *testing.T) {
+
+	images, err := ListChartImages("/usr/local/bin/helm", "../../toe-0.1.0.tgz", []string{"--values", "../../toe/values.yaml"})
+	if err != nil {
+		fmt.Println(err, "Error while listing chart images")
+	}
+	for _, image := range images {
+		if image.Tag == "v1.2.3" {
+			t.Fail()
+		}
+	}
+}
+
+func TestListImagesWithoutValues(t *testing.T) {
+
+	images, err := ListChartImages("/usr/local/bin/helm", "../../toe-0.1.0.tgz", []string{})
+	if err != nil {
+		fmt.Println(err, "Error while listing chart images")
+	}
+	containsRegistry := false
+	for _, image := range images {
+		if image.Tag == "v1.2.3" {
+			containsRegistry = true
+		}
+	}
+	if !containsRegistry {
+		t.Fail()
+	}
 }
